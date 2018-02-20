9de7c5fda67a1758963810b5b0d3dfe9d8e98b80
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-20 08:21:09 -0500

    created token validation for framework4.5

--- API/App_Start/WebApiConfig.cs
+++ API/App_Start/WebApiConfig.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
+using System.Web.Http.Cors;
 namespace API
 {
     public static class WebApiConfig
@@ -10,15 +8,34 @@ namespace API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var json = config.Formatters.JsonFormatter;
 
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //install Microsoft.AspNet.WebApi.Cors package first using nuget package management
+            //enabling cors
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            //for chrome to enable preflight request 
+            //--disable-web-security --user-data-dir
+
+            //to filter the cors
+            //import System.Web.Http.Cors to every controller you want to filter
+            //add annotation this to controller's class
+            //[EnableCorsAttribute("*","*","*")] 
+            //to disable some httpmethods 
+            //add annotation to method
+            //[DisableCors] 
         }
     }
 }
--- API/Migrations/Configuration.cs
+++ API/Migrations/Configuration.cs
@@ -1,12 +1,15 @@
 namespace API.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<API.Entities.SkillSetContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(API.Entities.SkillSetContext context)
