5a442f517dc194dbdce9a64d701caa23c426f10d
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-02-20 15:58:10 -0500

    test - - -

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace Client.Controllers
     public class HomeController : Controller
     {
         private string _authToken;
-        private string _apiToken;
         private HttpClient _client;
         //private TokenFactory _tokenFactory;
         public ActionResult Index()
@@ -23,10 +22,18 @@ namespace Client.Controllers
         public ActionResult SignIn()
         {
             //request a post to IDP server to gain an AuthToken
-            //getAuthentication();
-            NameValueCollection section = (NameValueCollection)ConfigurationManager.GetSection("secureAppSettings");
+            GetAuthentication();
+            NameValueCollection section = (NameValueCollection)ConfigurationManager.GetSection("appSettings");
             ViewData["homepage"] = section["ClientURL"];
             return View();
+        }
+
+        public void GetAuthentication()
+        {
+            ApiAccess api = new ApiAccess("Login");
+
+            var authToken = api.PostRequest("");
+            Session["authToken"] = authToken;
         }
     }
 }
